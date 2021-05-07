9c26c0021d26c235c61e913107cf0ad17cd27183
Eric Phifer <ericphifer@Erics-MacBook-Pro.local>
2021-05-07 12:18:15 -0600

    correct syntax of phone number to dial

--- gatsby/src/pages/boardmembers.js
+++ gatsby/src/pages/boardmembers.js
@@ -298,7 +298,7 @@ export default function BoardMembers({ data, pageContext }) {
                   id={member.id}
                   className="memberbtn call"
                 >
-                  <a href="tel:$(member.phone)">Call {member.name}</a>
+                  <a href={`tel:${member.phone}`}>Call {member.name}</a>
                 </button>
               </li>
             </ul>
@@ -342,7 +342,7 @@ export default function BoardMembers({ data, pageContext }) {
                     id={committee.id}
                     className="committeebtn call"
                   >
-                    <a href="tel:{committee.phone}">Call {committee.name}</a>
+                    <a href={`tel:${committee.phone}`}>Call {committee.name}</a>
                   </button>
                 </div>
                 <div className="container">
